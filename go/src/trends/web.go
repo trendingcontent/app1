15a60375d8f9e50a641a094322ac957e143688ad
Juan Valencia <trendingcontent@yahoo.com>
2013-03-20 01:25:45 -0700

    quick commit

--- go/src/trends/web.go
+++ go/src/trends/web.go
@@ -4,32 +4,18 @@ import (
     "fmt"
     "net/http"
     "os"
-    "time"
-    "log"
 )
 
 func main() {
-    /*
     http.HandleFunc("/", hello)
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
       panic(err)
     }
-    */
-    s := &http.Server{
-        Addr:           ":"+os.Getenv("PORT"),
-        Handler:        myHandler,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
-    log.Fatal(s.ListenAndServe())
 
 }
 
 func myHandler(res http.ResponseWriter, req *http.Request) {
     fmt.Fprintln(res, "hello, world")
 }
-
-
